8051987a61229b26bd52bff8fcc07d146e2d4e0a
Karam Ibrahim <karamzeroo@gmail.com>
2022-02-23 15:04:49 +0200

    commit :)
    Signed-off-by: Karam Ibrahim <karamzeroo@gmail.com>

--- app/src/main/java/com/example/rotatecounter/MainActivity.java
+++ app/src/main/java/com/example/rotatecounter/MainActivity.java
@@ -5,24 +5,49 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     int count=0;
+    public static final String countKey="count";
+    TextView view;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        if(savedInstanceState != null){
+            count=savedInstanceState.getInt(countKey);
+        }
+
         setContentView(R.layout.activity_main);
+
+        view = findViewById(R.id.counter);
+        view.setText(count+"");
+
+
     }
 
     @Override
-    public void onConfigurationChanged(@NonNull Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
-        TextView view = findViewById(R.id.counter);
-        count++;
-        view.setText(count+"");
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        outState.putInt(countKey,count);
+        super.onSaveInstanceState(outState);
+    }
 
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        count++;
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+       if(count>0)
+           count--;
     }
 }
